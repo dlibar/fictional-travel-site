4694a81d8824e83892d5332370dba04fcfca328f
Doune <d_libar@hotmail.com>
2018-09-05 08:28:09 +0100

    Completed desktop and mobile header styles.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,26 @@
+
+import MobileMenu from './modules/MobileMenu';
+
+var mobileMenu = new MobileMenu();
+
+/*var $ = require('jquery');
+//var Person = require('./modules/Person');
+
+import Person from './modules/Person';
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + " now owes $0.00 in taxes");
+	}
+}
+
+alert("Another new test ongoing");
+
+var doune = new Person("Doune Libar", "Brown");
+doune.greet();
+
+var rosanne = new Adult("Madjissem Kongar", "Orange");
+rosanne.greet();
+rosanne.payTaxes();
+
+$("h1").remove();*/
